1ceb4f37b48b1a66b9b10c77de55be7fcd0b1b78
tanuja0708 <tanujapandey398@gmail.com>
2026-07-17 17:26:36 +0530

    add middleware and login logout controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,22 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try{
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken= refreshToken
+        await user.save({validateBeforeSave : false})
+
+        return { accessToken, refreshToken}
+
+    }catch(error){
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async(req,res) => {
     //get user details from frontend -- here we will take it from postman
     //validation-- not empty
@@ -77,4 +93,86 @@ const registerUser = asyncHandler(async(req,res) => {
 
 })
 
-export {registerUser}
+
+const loginUser = asyncHandler(async(req,res) => {
+    //req.body-->data
+    //username or email
+    //find the user
+    //password check
+    //access and refresh token
+    //send cookie
+    //successfull login
+
+    const {email, username, password} = req.body
+
+    if(!username || !email){
+        throw new ApiError(400, " username or email is required")
+    }
+
+    const user = await User.findOne({                               //find One finds ythe first entry in the database and returns it
+        $or: [{username}, {email}]               //or operator is mongodb syntax which lets the function one the basis two given object
+    })
+
+    if(!user){
+        throw new ApiError(400, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshtoken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            }, 
+            "User loggedIn Successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async(req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {  //set is an operator in mongodb which updates the fields
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controllers.js";
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -16,5 +17,11 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+
+router.route("/logout").post(verifyJWT, logoutUser)   // as you can see post has two fns to run the next in verifyjWT tells the browser to move to the next fn
 
 export default router
