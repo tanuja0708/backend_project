0a8ecf15376088551db2be63d65dd39ce5758ada
tanuja0708 <tanujapandey398@gmail.com>
2026-07-21 20:59:47 +0530

    pm

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -244,7 +244,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res.
     status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res)=>
@@ -254,7 +254,7 @@ const updateAccountDetails = asyncHandler(async(req, res)=>
     if(!fullName && !email){
         throw new ApiError(400, "All fields are required")
     }
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
@@ -294,6 +294,7 @@ const updateUserAvatar = asyncHandler(async(req, res)=>
         {new: true}
     ).select("-password")
 
+    //todo -  delete the old avatar image --make an utility for it
     return res
     .status(200)
     .json(
